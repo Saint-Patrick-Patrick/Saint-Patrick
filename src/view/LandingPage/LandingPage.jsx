cedb681da73c640f9a552e3364660ce18cd401c5
ArceDaniel <jonathandanielarce9@gmail.com>
2023-03-08 12:52:38 -0300

    Back End Connection

--- src/view/LandingPage/LandingPage.jsx
+++ src/view/LandingPage/LandingPage.jsx
@@ -1,9 +1,16 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { getHello } from "../../features/apiPetitions";
 
 export default function LandingPage(){
+    const [hello, setHello] = useState(null);
+    useEffect(()=>{
+        getHello(setHello)
+    },[])
     return(
         <div>
             dani estuvo aqui
+            <br />
+            {hello}
         </div>
     )
 }
