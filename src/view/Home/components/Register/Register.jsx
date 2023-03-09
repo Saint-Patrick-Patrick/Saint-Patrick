a8d5062119ba96b63f27f8684f3402c96c1c5350
sonacho <nachokajita@gmail.com>
2023-03-09 15:13:40 -0300

    register user advances

--- src/view/Home/components/Register/Register.jsx
+++ src/view/Home/components/Register/Register.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
+import { registerUser } from '../../../../features/apiPetitions'
 import validationForm from './validator'
 
 export default function Register() {
 
     const [errors, setErrors] = useState()
     const [form, setForm]  = useState({
-        cardNum: null,
-        DNI: null,
+        email: '',
+        firstname: '',
+        lastname:'',
         password: '',
         confirmPassword:''
     })
@@ -20,17 +22,44 @@ export default function Register() {
     const handleSubmit = (e) =>{
         e.preventDefault()
         if (!Object.keys(errors).at(0)){
-            
+            delete form.confirmPassword
+            registerUser(form)
+            .then((data) => {console.log(data), alert('Usuario Registrado correctamente')})
+            .catch((e) => console.log(e))
+        }else{
+            alert('Debe completar todos los campos sin errores')
         }
     }
 
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name='cardNum' value={form.cardNum} onChange={handleChange}/>
-            <input type="text" name='DNI' value={form.DNI} onChange={handleChange}/>
-            <input type="password" name='password' value={form.password} onChange={handleChange}/>
-            <input type="password" name='confirmPassword' value={form.confirmPassword} onChange={handleChange}/>
+        <form onSubmit={handleSubmit} style={{display:'flex', flexDirection:'column', minHeight:'500px', justifyContent:'space-around'}}>
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <small>Nombre</small>
+                <input type="text" name='firstname' value={form.firstname} onChange={handleChange} placeholder='Nombre'/>
+                <small>{errors?.firstname}</small>
+            </div>
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <small>Apellido</small>
+                <input type="text" name='lastname' value={form.lastname} onChange={handleChange} placeholder='Apellido'/>
+                <small>{errors?.lastname}</small>
+            </div>
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <small>Correo</small>
+                <input type="text" name='email' value={form.email} onChange={handleChange} placeholder='Correo'/>
+                <small>{errors?.email}</small>
+            </div>
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <small>Contraseña</small>
+                <input type="password" name='password' value={form.password} onChange={handleChange} placeholder='Contraseña'/>
+                <small>{errors?.password}</small>
+            </div>
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <small>Confirmar Contraseña</small>
+                <input type="password" name='confirmPassword' value={form.confirmPassword} onChange={handleChange} placeholder='Confirmar Contraseña'/>
+                <small>{errors?.confirmPassword}</small>
+            </div>
+        
             <input type="submit" value={'Registrarse'}/>
         </form>
     </div>
--- src/view/Home/components/Register/validator.js
+++ src/view/Home/components/Register/validator.js
@@ -1,6 +1,6 @@
 const validationForm = {
     errors: {},
-    cardNum: (form) => {
+    /* cardNum: (form) => {
         if(form.cardNum.length!==16){
             validationForm.errors.cardNum = 'Introduza un múmero de tarjeta válido'
         }else if(!form.cardNum){
@@ -23,6 +23,44 @@ const validationForm = {
             delete validationForm.errors.DNI
         }
         return validationForm.errors
+    }, */
+    firstname:(form) => {
+        if(form.firstname.length<2){
+            validationForm.errors.firstname = 'El nombre debe ser mas largo que 2 letras'
+        }else if(form.firstname.length>20){
+            validationForm.errors.firstname = 'El nombre debe tener menos de 20 letras de largo'
+        }else if(!form.firstname){
+            validationForm.errors.firstname = 'El nombre no puede estar vacio'
+        }else if(!form.firstname.match(/^(?=.{2,20}$)[a-z]+(?:['_.\s][a-z]+)*$/i)){
+            validationForm.errors.firstname = "El nombre no puede tener caracteres especiales o espacios antes y después del nombre"
+        }else{
+            delete validationForm.errors.firstname
+        }
+        return validationForm.errors
+    },
+    lastname: (form) => {
+        if(form.lastname.length<4){
+            validationForm.errors.lastname = 'El apellido debe ser mas largo que 4 letras'
+        }else if(form.lastname.length>20){
+            validationForm.errors.lastname = 'El apellido debe tener menos de 20 letras de largo'
+        }else if(!form.lastname){
+            validationForm.errors.lastname = 'El apellido no puede estar vacio'
+        }else if(!form.lastname.match(/^(?=.{4,20}$)[a-z]+(?:['_.\s][a-z]+)*$/i)){
+            validationForm.errors.lastname = "El nombre no puede tener caracteres especiales o espacios antes y después del nombre"
+        }else{
+            delete validationForm.errors.lastname
+        }
+        return validationForm.errors
+    },
+    email:  (form) => {
+        if(!form.email){
+            validationForm.errors.email = 'El email no puede estar vacio'
+        }else if(!form.email.match(/^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/)){
+            validationForm.errors.email = "Debe introducir un formato de e-mail válido"
+        }else{
+            delete validationForm.errors.email
+        }
+        return validationForm.errors
     },
     password: (form) => {
         if(!form.password){
@@ -32,7 +70,7 @@ const validationForm = {
         }else if(form.password.length>25){
             validationForm.errors.password = 'La contraseña debe tener un largo menor a 25 caracteres'
         }else if(!/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,26}$/.test(form.password)){
-            validationForm.errors.password = `El repetir contraseña debe tener al menos 1 letra minúscula, 1 numero y 1 letra Mayúscula`
+            validationForm.errors.password = `La contraseña debe tener al menos 1 letra minúscula, 1 numero y 1 letra Mayúscula`
         }else{
             delete validationForm.errors.password
         }
