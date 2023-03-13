cc3f81f9447203e04948cfa7a67667b0be680389
Ivette1792 <ivettefont179@gmail.com>
2023-03-13 14:56:49 -0300

    login

--- src/view/Login/Login.jsx
+++ src/view/Login/Login.jsx
@@ -1,6 +1,28 @@
-export default function Login(){
+import React from 'react';
+import './Login.css';
+import './global.css';
+import 'font-awesome/css/font-awesome.min.css';
 
-    return(
-        <div>Login</div>
-    )
+function Login() {
+return (
+    <div>
+    <h1>Inicio de Sesión</h1>
+    <form>
+        <img src="img/Logosaint.png" alt="Logo de la empresa" />
+        <input type="text" id="correo" name="correo" placeholder="Ingresa tu correo electrónico o documento" />
+        <label htmlFor="correo">Login</label>
+        <div className="password-container">
+        <input type="password" id="contrasena" name="contrasena" placeholder="Ingresa tu contraseña" />
+        <label htmlFor="contrasena">Password</label>
+        <a href="#" style={{float: 'right'}}>¿Olvidaste tu Contraseña?</a>
+        </div>
+        <br />
+        <button className="btn-google">Iniciar con Google</button>
+        <button className="btn-facebook">Iniciar con Facebook</button>
+        <button className="btn-submit">Iniciar sesión</button>
+    </form>
+    </div>
+);
 }
+
+export default Login;
