6d3e55ab3268f22c1003b139489b458a3fbe0034
rociodepoli <rociodepoli69@gmail.com>
2023-03-24 20:05:58 -0300

    help

---
+++ src/features/transactionsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  transactions:null
+};
+
+const transactionsSlice = createSlice({
+  name: "transactions",
+  initialState,
+  reducers: {
+    setTransactions(state, { payload }) {
+      state.transactions = payload;
+      console.log(state);
+    },
+  },
+});
+export const { setTransactions } = transactionsSlice.actions;
+export default transactionsSlice.reducer;
