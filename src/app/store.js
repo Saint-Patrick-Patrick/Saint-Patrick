b3f3cf20a0d7540ecef3d23725afeef6851ef79d
sonacho <nachokajita@gmail.com>
2023-03-24 20:35:28 -0300

    fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import transactionsSlice from "../features/transactionsSlice";
 import userSlice from "../features/userSlice";
 
 export default configureStore({
   reducer: {
     user: userSlice,
+    transactions:transactionsSlice
   },
 });
